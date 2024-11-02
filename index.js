4e06d98c02a45cbd3b17781f60aa63fa4aa0a8ac
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-11-02 03:52:43 -0700

    Add button to get new gif

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=IIpHuHgFhbRo54JLaw9GvFo8HaCWeIeH&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+
+function getNewCatGif() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=IIpHuHgFhbRo54JLaw9GvFo8HaCWeIeH&s=cats",
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+const refreshButton = document.getElementById("refresh-button");
+refreshButton.addEventListener('click', (e) => {
+    getNewCatGif();
+});
+
+getNewCatGif();
